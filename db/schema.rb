10624418d256a192f821c597f975679b438904b0
mirv <reski.raptor@gmail.com>
2016-12-25 05:02:42 +0000

    adding db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214133854) do
+ActiveRecord::Schema.define(version: 20161223192039) do
 
   create_table "continents", force: :cascade do |t|
     t.string   "name"
