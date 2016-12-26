031223c2e71da0e8e6e16d6006ac84149a76b7c4
mirv <reski.raptor@gmail.com>
2016-12-26 15:27:42 +0000

    Added callback for removal, also pulled .on('region') selector as unnecessary

--- app/assets/javascripts/continents.js
+++ app/assets/javascripts/continents.js
@@ -3,36 +3,32 @@ $(document).on("ready", function() {
   
   $(document).on('cocoon:before-insert', 'form', function(e,region_to_be_added) {
     console.log(region_to_be_added);
-    console.log("Regions before-insert");
+    console.log("Regions - before-insert");
     e.stopPropagation();
     region_to_be_added.fadeIn('slow');
   });
   $(document).on('cocoon:after-insert', 'form', function(e,region_to_be_added) {
     console.log(region_to_be_added);
-    console.log("Regions after-insert");
+    console.log("Regions - after-insert");
     e.stopPropagation();
     region_to_be_added.fadeIn('slow');
   });
 
-  $(document).on('cocoon:before-insert', 'regions', function(e,country_to_be_added) {
-    console.log(country_to_be_added);
-    console.log("Countries before-insert");
+
+  $('.regions').on('cocoon:before-remove', function(e, region_to_remove) { 
+    console.log("Remove Region");
+    $(this).data('remove-timeout', 1000); 
+    region_to_remove.fadeOut('slow'); 
     e.stopPropagation();
-    country_to_be_added.fadeIn('slow');
-  });
-  $(document).on('cocoon:after-insert', 'regions', function(e,country_to_be_added) {
-    console.log(country_to_be_added);
-    console.log("Countries after-insert");
+    });
+    
+  $('.countries').on('cocoon:before-remove', function(e, country_to_remove) { 
+    console.log("Remove Country");
+    $(this).data('remove-timeout', 1000); 
+    country_to_remove.fadeOut('slow'); 
     e.stopPropagation();
-    country_to_be_added.fadeIn('slow');
   });
 
-
-// Adapt this ... 1) bind -> on 2) plural region with #
-$('#regions').on('cocoon:before-remove', function(e, region_to_remove) { 
-  $(this).data('remove-timeout', 1000); 
-  region_to_remove.fadeOut('slow'); 
-  });
 
 });
   
